9ee28ddfafe1c404193831b279a8301587318c53
Thanaporn Jampathip <tanaporn7249.kk@gmail.com>
2026-01-18 01:42:12 +0700

    fixed

--- pages/tables/allUserTable.php
+++ pages/tables/allUserTable.php
@@ -9,7 +9,7 @@ $queryUsers = mysqli_query($conn, $sqlUsers);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/tables/adminTable.css">
+    <link rel="stylesheet" href="../css/tables/allUserTable.css">
     <title>Document</title>
 </head>
 
