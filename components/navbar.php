fa5f629ff2cb353ce2c5aef6c9b53ee8d4ab4bdb
Thanaporn Jampathip <tanaporn7249.kk@gmail.com>
2026-01-18 03:50:34 +0700

    fixed

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/components/navbar.css">
     <title>Document</title>
 </head>
+
 <body>
     <nav>
         <div class="user">
             <!-- link -->
-            <h6><i class="bi bi-person-fill"></i><?php echo $rowUser['username'] ?></h6>
+            <div class="link">
+                <a href="./account.php"><i class="bi bi-person-fill"></i><?php echo $rowUser['username'] ?></a>
+            </div>
         </div>
     </nav>
 </body>
+
 </html>
